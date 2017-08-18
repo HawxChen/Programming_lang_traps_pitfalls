7a5faa4b6bef865c71e179c333383daaba87e575
HawxChen <yingjheng.chen@gmail.com>
2017-08-18 11:57:40 -0700

    related string char array

--- CPP/02.string_char_array.cc
+++ CPP/02.string_char_array.cc
@@ -4,20 +4,17 @@ using namespace std;
 int main() {
     string s = "123566";
     s[0] = '\0';
+    s[1] = '\0';
     s.push_back('C');
 
     printf("\"%s\"\n", s.c_str());
-    //The output will be "" instead of "C";
+    //The output will be "".
 
     cout << "\"" << s.c_str() << "\"" << endl;
-    //The output will be "" instead of "C";
+    //The output will be "".
 
     cout << "\"" << s << "\"" << endl;
-    //The output will be "23566C" instead of "C";
+    //The output will be "3566C".
     
     return 0;
 }
-
-/*
- *Thanks for yoshijava's discussion.
- */
--- CPP/04.string_char_array.cc
+++ CPP/04.string_char_array.cc
@@ -4,20 +4,17 @@ using namespace std;
 int main() {
     string s = "123566";
     s[0] = '\0';
-    s.push_back('C');
+    s[1] = '\0';
+    s[0] = 'C';
 
     printf("\"%s\"\n", s.c_str());
-    //The output will be "" instead of "C";
+    //The output will be "C".
 
     cout << "\"" << s.c_str() << "\"" << endl;
-    //The output will be "" instead of "C";
+    //The output will be "C".
 
     cout << "\"" << s << "\"" << endl;
-    //The output will be "23566C" instead of "C";
+    //The output will be "C3566";
     
     return 0;
 }
-
-/*
- *Thanks for yoshijava's discussion.
- */

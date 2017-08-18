54536b6930788bed664ea559f7238ab5d1a3f1c7
HawxChen <yingjheng.chen@gmail.com>
2017-08-18 11:42:00 -0700

    cpp: char array and string

---
+++ CPP/01.string_char_array.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main() {
+    string s = "123566";
+    s[0] = '\0';
+    s.push_back('C');
+
+    puts(s.c_str());
+    //The output will be "" instead of "C";
+
+    cout << s.c_str() << endl;
+    //The output will be "" instead of "C";
+
+    cout << s << endl;
+    //The output will be "23566C" instead of "C";
+    
+    return 0;
+}
